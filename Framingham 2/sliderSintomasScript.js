339574fdb46c3b7c457ba9d767831f194f1c7c03
Eduardo Salas Jaramillo <149119842+edsaja18@users.noreply.github.com>
2024-04-09 16:54:55 -0600

    Add files via upload

---
+++ Framingham 2/sliderSintomasScript.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const galleryContainer = document.querySelector('.gallery-container');
+    const totalImages = document.querySelectorAll('.gallery-item').length;
+    let currentIndex = 0;
+ 
+    function autoSlide() {
+        currentIndex = (currentIndex + 1) % totalImages;
+        const offset = -currentIndex * 100;
+        galleryContainer.style.transition = "transform 0.5s ease-in-out"; // Agrega transición suave
+        galleryContainer.style.transform = `translateX(${offset}%)`;
+    }
+ 
+    function resetSlide() {
+        if (currentIndex === totalImages - 1) {
+            setTimeout(() => {
+                galleryContainer.style.transition = "none"; // Desactiva la transición para el reset
+                currentIndex = 0;
+                const offset = -currentIndex * 100;
+                galleryContainer.style.transform = `translateX(${offset}%)`;
+                setTimeout(() => {
+                    galleryContainer.style.transition = "transform 0.5s ease-in-out"; // Vuelve a activar la transición
+                }, 0);
+            }, 500); // Ajusta el tiempo para dar un pequeño espacio entre las transiciones
+        }
+    }
+ 
+    setInterval(autoSlide, 5000); // Ajusta el intervalo de cambio automático de imagen (en milisegundos)
+    galleryContainer.addEventListener('transitionend', resetSlide); // Restablece la posición después de la transición
+ });
+
+ 
+ 
